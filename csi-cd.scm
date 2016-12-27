84ea34f3a73eadfaee479b6af63c8a8963fe770b
Nicola Archibald <nicola.archibald@gmail.com>
2016-12-27 13:03:55 +0000

    Fixed import/use issues

--- csi-cd.scm
+++ csi-cd.scm
@@ -1,20 +1,33 @@
 (module csi-cd
   ()
-  (import chicken scheme extras
-          (only csi toplevel-command)
-          (only srfi-13 string-trim-both)
-          (only posix change-directory current-directory))
+  (import chicken scheme
+          (only csi toplevel-command))
 
+
+  (use (only extras read-line)
+       (only srfi-13 string-trim-both)
+       (only posix change-directory current-directory))
+
+
+  (define (cwd p)
+    (change-directory p))
+
+  (define (gwd)
+    (current-directory))
+
+  (define (trim string)
+    (string-trim-both string))
+  
 (toplevel-command 'cd
    (lambda ()
      (let* [(rest (string-trim-both (read-line)))]
-       (change-directory rest)
-       (print (current-directory))))
+       (cwd rest)
+       (print (gwd))))
    ",cd DIRECTORY     Change working directory for csi")
 
 (toplevel-command 'pwd
    (lambda ()
-     (print (current-directory))
+     (print (gwd))
      )
    ",pwd              Display current working directory")
 )
